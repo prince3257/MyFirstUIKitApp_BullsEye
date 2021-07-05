38e42ec48792ee4a0d5074fb4c4507408bbf18c9
Daniel Lee <prince3257@gmail.com>
2021-07-05 19:44:22 +0800

    - Adding function of slider - update alert content(target value & slider value) - update target value at interface with "random target value" showing

--- MyFirstUIKitApp_BullsEye/ViewController.swift
+++ MyFirstUIKitApp_BullsEye/ViewController.swift
@@ -9,25 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var showtarget: UILabel!
+    
+    override func viewDidLoad() { //is called as soon as ViewController loads its interface
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
     }
     
     //to tell the swift to connect with something in storyboard.
     @IBAction func showAlert() {
         
-        let alert = UIAlertController(title: "Hello World. :)", message: "This is my first app!", preferredStyle: .alert)
+        let message = "The value of the slider is now: \(currentValue)" + "\nThe target valu is: \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello World. :)", message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome!", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     
     }
     
+    @IBAction func sliderMoved(_ slider: UISlider){
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
    
+    func updateLabels(){
+        showtarget.text = String(targetValue)
+        
+    }
     
 
 }
