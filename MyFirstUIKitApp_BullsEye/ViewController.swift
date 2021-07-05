ad24c9d65bba6f59758c154c0e05481c2b16ddb4
Daniel Lee <prince3257@gmail.com>
2021-07-05 15:06:58 +0800

    For testing only, but congrats! This is your first and official App:)

--- MyFirstUIKitApp_BullsEye/ViewController.swift
+++ MyFirstUIKitApp_BullsEye/ViewController.swift
@@ -13,7 +13,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+       
+    /*
+     This is for testing only!
+     Congrats Daniel, you've tried to use git on your own:)
+     Keep working on it!!!
+     You'll succeed.
+     */
 
 }
 
